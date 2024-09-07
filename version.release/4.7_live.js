84599d6946b76d18ab71d12bcb9bf4c6a52fc68b
Roslyn <150500547+GesthosNetwork@users.noreply.github.com>
2024-09-07 19:36:45 +0700

    Update 4.7_live.js

--- version.release/4.7_live.js
+++ version.release/4.7_live.js
@@ -2,9 +2,29 @@ const list =
 {
 	'4.7_live': [
 		{
-			res: {Version: 23760009, Suffix: "e0c5214301"},
-			clientSilence: {Version: 23983991, Suffix: "dc1883d0e9"},
+			client: {Version: 23781031, Suffix: "458e047c08"},
+		},
+		
+		{
+			clientSilence: {Version: 23876922, Suffix: "db530509c3"},
 			client: {Version: 23935420, Suffix: "cddf437c96"},
+		},
+		
+		{
+			clientSilence: {Version: 23983991, Suffix: "dc1883d0e9"},
+			client: {Version: 24108674, Suffix: "557571de1f"},
+		},
+		
+		{
+			res: {Version: 23760009, Suffix: "e0c5214301"},
+			clientSilence: {Version: 24199308, Suffix: "3b4747f5b3"},
+			client: {Version: 24361154, Suffix: "2c6aa68ebd"},
+		},
+		
+		{
+			res: {Version: 24292157, Suffix: "35b707301e"},
+			clientSilence: {Version: 24332145, Suffix: "492068e4a6"},
+			client: {Version: 24803766, Suffix: "5e5f470e3e"},
 		},
 	],
 }
