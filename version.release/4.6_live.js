25ef7f1a442043f944a99f9f548907d548051ab7
Roslyn <150500547+GesthosNetwork@users.noreply.github.com>
2024-09-07 19:36:08 +0700

    Add files via upload

---
+++ version.release/4.6_live.js
@@ -0,0 +1,23 @@
+const list =
+{
+	'4.6_live': [
+		{
+			clientSilence: {Version: 22543584, Suffix: "8ea3d42e6b"},
+			client: {Version: 22626513, Suffix: "d47876295c"},
+		},
+		
+		{
+			res: {Version: 22608561, Suffix: "fcbb017dbc"},
+			clientSilence: {Version: 000, Suffix: "000"},
+			client: {Version: 22846342, Suffix: "d8445c0b38"},
+		},
+		
+		{
+			res: {Version: 22943756, Suffix: "30aa0850f9"},
+			clientSilence: {Version: 23185664, Suffix: "83ab7ef56e"},
+			client: {Version: 23160011, Suffix: "2a6959f898"},
+		},
+	],
+}
+
+module.exports = {list}
---
+++ version.release/4.8_live.js
@@ -0,0 +1,27 @@
+const list =
+{
+	'4.8_live': [
+		{
+			client: {Version: 25202536, Suffix: "e993e656e7"},
+		},
+
+		{
+			res: {Version: 24855007, Suffix: "a648e76073"},
+			client: {Version: 25329614, Suffix: "4816d27134"},
+		},
+
+		{
+			res: {Version: 25462395, Suffix: "ac5b7b72db"},
+			clientSilence: {Version: 25275248, Suffix: "f48d7eab98"},
+			client: {Version: 25518246, Suffix: "c61e7a470e"},
+		},
+
+		{
+			res: {Version: 25683502, Suffix: "e97e19ed63"},
+			clientSilence: {Version: 25353456, Suffix: "5cf8b1c426"},
+			client: {Version: 25722854, Suffix: "8853cb1f62"},
+		},
+	],
+}
+
+module.exports = {list}
